41875c6f194a1dfd43a2b8b835f6b64d9170561e
m-ishizaki <love.ms.is12t@hotmail.co.jp>
2017-06-18 01:07:16 +0900

    イベントの情報を ListBox に表示

--- DialTest/MainPage.xaml.cs
+++ DialTest/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Input;
@@ -21,55 +24,157 @@ namespace DialTest
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        // Surface Dial の Controller インスタンス
+        private RadialController _controller;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             DialSettings();
+            this.DataContext = this;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Dial の操作の記録
+        /// </summary>
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+
+        /// <summary>
+        /// Dial の操作を記録します
+        /// </summary>
+        /// <param name="cont"></param>
+        /// <param name="menu"></param>
+        /// <param name="event"></param>
+        /// <param name="arg"></param>
+        private void AddItem(RadialControllerScreenContact cont, RadialControllerMenuItem menu, string @event, string arg)
+        {
+            Items.Insert(0, $"{cont?.Position.ToString()} : {menu?.DisplayText} : {@event} : {arg}");
+            if (1000 < Items.Count) Items.RemoveAt(1000);
+        }
+
+        /// <summary>
+        /// Dial の操作を記録します
+        /// </summary>
+        /// <param name="cont"></param>
+        /// <param name="ctrl"></param>
+        /// <param name="event"></param>
+        /// <param name="arg"></param>
+        private void AddItem(RadialControllerScreenContact cont, RadialController ctrl, string @event, string arg)
+            => AddItem(cont, ctrl?.Menu?.GetSelectedMenuItem(), @event, arg);
 
         private void DialSettings()
         {
-            // コントローラー
-            var controller = RadialController.CreateForCurrentView();
+            // コントローラーを取得
+            var controller = _controller = RadialController.CreateForCurrentView();
 
-            // メニューの設定
-            var configration = RadialControllerConfiguration.GetForCurrentView();
-            var menuItem = RadialControllerMenuItem.CreateFromKnownIcon("Item Name", RadialControllerMenuKnownIcon.InkColor);
-            controller.Menu.Items.Add(menuItem);
-            configration.SetDefaultMenuItems(Enumerable.Empty<RadialControllerSystemMenuItemKind>());
-            configration.SetDefaultMenuItems(new[] { RadialControllerSystemMenuItemKind.Volume, RadialControllerSystemMenuItemKind.Scroll });
+            {// アプリ独自メニューの設定
+                var menuItems = new List<RadialControllerMenuItem>(){
+                RadialControllerMenuItem.CreateFromKnownIcon("Item Name 1", RadialControllerMenuKnownIcon.InkColor),
+                RadialControllerMenuItem.CreateFromKnownIcon("Item Name 2", RadialControllerMenuKnownIcon.PenType) };
+                menuItems.ForEach(m => controller.Menu.Items.Add(m));
+                // メニューが選択されたイベント
+                menuItems.ForEach(m => m.Invoked += (sender, arg) => AddItem(
+                    null,
+                    sender,
+                    nameof(RadialControllerMenuItem.Invoked),
+                    arg?.ToString()
+                    )
+                );
+            }
+            {// 標準メニューの設定
+                // ここでは音量とスクロールだけを設定している
+               var configration = RadialControllerConfiguration.GetForCurrentView();
+                //configration.SetDefaultMenuItems(Enumerable.Empty<RadialControllerSystemMenuItemKind>());
+                configration.SetDefaultMenuItems(new[] { RadialControllerSystemMenuItemKind.Volume, RadialControllerSystemMenuItemKind.Scroll });
+            }
 
-            // イベント
-            menuItem.Invoked += async (sender, args) =>
-            {
-                var selected = controller.Menu.GetSelectedMenuItem();
-                var md = new Windows.UI.Popups.MessageDialog(selected?.DisplayText, sender?.DisplayText);
-                await md.ShowAsync();
-            };
+            // イベントハンドラの設定（イベントの記録）
 
-            controller.ControlLost += async (sender, arg) =>
-            {
-                var selected = controller.Menu.GetSelectedMenuItem();
-                var md = new Windows.UI.Popups.MessageDialog(selected?.DisplayText, sender?.Menu?.GetSelectedMenuItem()?.DisplayText);
-                await md.ShowAsync();
-            };
+            // 標準メニューを選択した際や
+            // アプリがフォーカスを失った時にも発生
+            controller.ControlLost += (sender, arg) => AddItem(
+                null,
+                sender,
+                nameof(RadialController.ControlLost),
+                arg?.ToString()
+                );
 
-            controller.RotationChanged += async (sender, arg) =>
-            {
-                var selected = controller.Menu.GetSelectedMenuItem();
-                var md = new Windows.UI.Popups.MessageDialog(selected?.DisplayText, arg?.RotationDeltaInDegrees.ToString());
-                await md.ShowAsync();
-            };
+            // Dial を回転したときに発生
+            // arg.IsButtonPressed で押し込みながらの回転かを判別できる
+            controller.RotationChanged += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.RotationChanged),
+                $"{nameof(arg.IsButtonPressed)} : {arg.IsButtonPressed}, {nameof(arg.RotationDeltaInDegrees)} : {arg.RotationDeltaInDegrees}"
+                );
 
-            controller.ButtonClicked += async (sender, arg) =>
-            {
-                var selected = controller.Menu.GetSelectedMenuItem();
-                var md = new Windows.UI.Popups.MessageDialog(selected?.DisplayText, arg?.ToString());
-                await md.ShowAsync();
-            };
+            // Dial のクリック（押し込み）時に発生
+            // クリック時、ButtonPressed ⇒ ButtonReleased ⇒ ButtonClicked と 3 つが続けて発生
+            // 押し込みながら Dial 回転した後でも ButtonClicked が発生する
+            controller.ButtonClicked += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ButtonClicked),
+                arg?.ToString()
+                );
+
+            controller.ButtonHolding += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ButtonHolding),
+                arg?.ToString()
+                );
+
+            // Dial を押し込んだ際に発生
+            controller.ButtonPressed += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ButtonPressed),
+                arg?.ToString()
+            );
+
+            // Dial を押し込んだ後、離した際に発生
+            controller.ButtonReleased += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ButtonReleased),
+                arg?.ToString()
+                );
+
+            // 標準メニュー選択状態から独自メニューを選択した際や
+            // アプリがフォーカスを得た際にも発生
+            controller.ControlAcquired += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ControlAcquired),
+                arg?.ToString()
+                );
+
+            controller.ScreenContactContinued += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ScreenContactContinued),
+                arg?.ToString()
+                );
+
+            controller.ScreenContactEnded += (sender, arg) => AddItem(
+                null,
+                sender,
+                nameof(RadialController.ScreenContactEnded),
+                arg?.ToString()
+                );
+
+            controller.ScreenContactStarted += (sender, arg) => AddItem(
+                arg.Contact,
+                sender,
+                nameof(RadialController.ScreenContactStarted),
+                $"{nameof(arg.IsButtonPressed)} : {arg?.IsButtonPressed}, {arg?.ToString()}"
+                );
         }
     }
 }
